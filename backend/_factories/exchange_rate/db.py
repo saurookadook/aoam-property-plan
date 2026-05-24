2b0de72c82cdbe0b12270dc9c3125df996315ac0
Andy Maskiell <maskiella@gmail.com>
2026-05-24 17:16:22 -0400

    feat: add test for exchange_rates

---
+++ backend/_factories/exchange_rate/db.py
@@ -0,0 +1,26 @@
+# pyright: reportIncompatibleVariableOverride=false
+from __future__ import annotations
+
+from datetime import timedelta
+from random import randint
+
+import factory
+
+from _factories.mixins.db import TimestampsDBMixinFactory
+from db.db_session_manager import DBSessionManager
+from models.exchange_rate.db import ExchangeRateDB
+
+
+class ExchangeRateDBFactory(
+    TimestampsDBMixinFactory, factory.alchemy.SQLAlchemyModelFactory
+):
+    class Meta:
+        model = ExchangeRateDB
+        sqlalchemy_session = DBSessionManager().scoped_session
+
+    record_date = factory.LazyAttribute(
+        lambda o: o.created_at - timedelta(minutes=randint(0, 600))
+    )
+    cop_per_usd = factory.Faker(
+        "pyfloat", positive=True, min_value=0.01, max_value=5000
+    )
