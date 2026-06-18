ead2a720750afbb6b878726038b6a54aa4afc88f
Andy Maskiell <maskiella@gmail.com>
2026-06-18 10:26:39 +0000

    chore: clean up ListingDBFactory

--- backend/_factories/listing/db.py
+++ backend/_factories/listing/db.py
@@ -10,22 +10,6 @@ from _factories.mixins.db import TimestampsDBMixinFactory
 from db.db_session_manager import DBSessionManager
 from models.listing.db import ListingDB
 
-# from rich import inspect as ri
-
-
-# def parse_value_from_point(point_str: str, index: int):
-#     try:
-#         point_str = point_str.strip()
-#         if point_str.startswith("POINT(") and point_str.endswith(")"):
-#             point_str = point_str[6:-1]
-#             parts = point_str.split()
-#             if len(parts) == 2:
-#                 return float(parts[index])
-#     except Exception as e:
-#         raise ValueError(f"Invalid POINT string format: {point_str}") from e
-
-#     raise ValueError(f"Invalid POINT string format: {point_str}")
-
 
 class ListingDBFactory(
     TimestampsDBMixinFactory,
