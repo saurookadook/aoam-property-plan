c20387669766b55bbfaf45273dba76d2389871be
Andy Maskiell <maskiella@gmail.com>
2026-06-18 11:38:44 +0000

    feat: install other dependencies, set up vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import path from 'path';
+import { defineConfig, type LogLevel, type UserConfig } from 'vite';
+import type { InlineConfig } from 'vitest/node';
+import react from '@vitejs/plugin-react';
+
+const __dirname = path.resolve();
+
+const ViteLogLevels = {
+  INFO: 'info',
+  WARN: 'warn',
+  ERROR: 'error',
+  SILENT: 'silent',
+} as const;
+
+const { LOG_LEVEL, SERVER_PORT } = process.env;
+
+const configLogLevel =
+  LOG_LEVEL != null && LOG_LEVEL in ViteLogLevels // force formatting
+    ? (LOG_LEVEL as LogLevel)
+    : 'info';
+
+type ViteConfig = UserConfig & { test: InlineConfig; logLevel?: LogLevel };
+
+const config: ViteConfig = {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  test: {
+    alias: {
+      /* Fixes for module resolutions */
+      '#saurookkadookk/react-utils-render-with-context': path.resolve(
+        __dirname,
+        '../node_modules/@saurookkadookk/react-utils-render-with-context/dist/esm',
+      ),
+    },
+    environment: 'jsdom',
+    include: [
+      '**/*.{test,spec}.{js,jsx,ts,tsx}', // force formatting
+      '**/__tests__/**/*.{ts,tsx}',
+    ],
+    reporters: ['verbose'],
+    sequence: {
+      hooks: 'list',
+    },
+    setupFiles: ['./vitest.setup.ts'],
+  },
+};
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(config);
