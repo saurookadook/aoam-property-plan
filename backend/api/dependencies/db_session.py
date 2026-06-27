d0a52afa2afc8b7bea630f7d37dccfedd2953c8e
Andy Maskiell <maskiella@gmail.com>
2026-06-27 13:04:21 -0400

    feat: add types to db_session dependency

--- backend/api/dependencies/db_session.py
+++ backend/api/dependencies/db_session.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+from typing import Annotated, Any, Generator
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
 from db.db_session_manager import DBSessionManager
 
+DBSessionGenerator = Generator[Session, Any, None]
 
-def db_session():
+
+def db_session() -> DBSessionGenerator:
     """FastAPI dependency to get database session
 
     Yields:
@@ -18,3 +25,6 @@ def db_session():
         raise
     finally:
         DBSessionManager().scoped_session.remove()
+
+
+DBSessionDependency = Annotated[DBSessionGenerator, Depends(db_session)]
