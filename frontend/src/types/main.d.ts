5d992691f731cc2e72624b1b03bcd52cd0a84545
Andy Maskiell <maskiella@gmail.com>
2026-06-27 11:44:55 -0400

    feat: add storybookEntry

--- frontend/src/types/main.d.ts
+++ frontend/src/types/main.d.ts
@@ -1,3 +1,15 @@
+export type KeyedObject<V = any, K = string> = {
+  [key in K]: V;
+};
+
+export type NullableValue<T> = T | null | undefined;
+
 export type ValueOf<T> = T[keyof T];
+
+export type BoundThis = {
+  name?: string;
+};
+
+export type FixedLengthArray<T, L extends number> = [T, ...T[]] & { length: L };
 
 export * from './react-custom';
