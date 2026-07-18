6ad221e5b0b28084af17a3d8c4118cb810f80097
Andy Maskiell <maskiella@gmail.com>
2026-07-18 10:50:49 -0400

    feat: add leaftlet map to ListingOverview page

--- frontend/src/pages/listings/ListingOverview/components/ListingOverviewData/index.tsx
+++ frontend/src/pages/listings/ListingOverview/components/ListingOverviewData/index.tsx
@@ -1,4 +1,5 @@
 import { Link as RouterLink } from 'react-router';
+import classNames from 'classnames';
 import { Masonry } from '@mui/lab';
 import {
   Card,
@@ -8,32 +9,62 @@ import {
   Paper,
   Typography,
 } from '@mui/material';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 import type { ListingEntity } from '@/types';
-import { FlexColumn } from '@/layouts';
+import { FlexColumn, FlexRow } from '@/layouts';
 
 import './styles.scss';
 
 export function ListingOverviewData({ listing }: { listing: ListingEntity }) {
+  const latLngPosition: [number, number] = [
+    listing?.latitude ?? 0,
+    listing?.longitude ?? 0,
+  ];
+
   return (
     <FlexColumn className="listing-overview-data">
-      <Card key={listing.id} className="listing-overview-data__data-item">
-        <CardContent>
-          <Typography variant="h2">{listing?.name ?? 'Name missing 🤷‍♂️'}</Typography>
+      <FlexRow className="listing-overview-data__top-row">
+        <Card className="listing-overview-data__data-item">
+          <CardContent>
+            <Typography variant="h2">{listing?.name ?? 'Name missing 🤷‍♂️'}</Typography>
 
-          <Typography
-            className="listing-overview-data__data-item__details-wrapper"
-            variant="body2"
-          >
             <Typography
-              className="listing-overview-data__data-item__description"
-              component="span"
+              className="listing-overview-data__data-item__details-wrapper"
+              variant="body2"
             >
-              {listing?.description ?? 'No description 🤷‍♂️'}
+              <Typography
+                className="listing-overview-data__data-item__description"
+                component="span"
+              >
+                {listing?.description ?? 'No description 🤷‍♂️'}
+              </Typography>
             </Typography>
-          </Typography>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+
+        <Card className={classNames('listing-overview-data__map', 'map-card')}>
+          <CardContent>
+            <MapContainer
+              id="listing-overview-map"
+              center={latLngPosition}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <Marker position={latLngPosition}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </CardContent>
+        </Card>
+      </FlexRow>
     </FlexColumn>
   );
 }
