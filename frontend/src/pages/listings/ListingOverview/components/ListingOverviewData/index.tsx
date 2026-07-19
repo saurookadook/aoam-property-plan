5ea7f194727e4f42608182b46b4b256113668c4e
Andy Maskiell <maskiella@gmail.com>
2026-07-19 10:12:45 -0400

    feat: refactor ListingOverviewData component

--- frontend/src/pages/listings/ListingOverview/components/ListingOverviewData/index.tsx
+++ frontend/src/pages/listings/ListingOverview/components/ListingOverviewData/index.tsx
@@ -1,73 +1,65 @@
+import { useMemo } from 'react';
 import { Link as RouterLink } from 'react-router';
-import classNames from 'classnames';
-import DOMPurify from 'dompurify';
-import { Masonry } from '@mui/lab';
-import {
-  Card,
-  CardActions,
-  CardContent,
-  Divider,
-  Paper,
-  Typography,
-} from '@mui/material';
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { Card, CardContent, Divider, Paper, Typography } from '@mui/material';
 
 import type { ListingEntity } from '@/types';
 import { FlexColumn, FlexRow } from '@/layouts';
+import { ListingDescriptionCard } from '../ListingDescriptionCard';
+import { ListingMapCard } from '../ListingMapCard';
+import { ListingPhotosCarousel } from '../ListingPhotosCarousel';
 
 import './styles.scss';
 
 export function ListingOverviewData({ listing }: { listing: ListingEntity }) {
-  const latLngPosition: [number, number] = [
-    listing?.latitude ?? 0,
-    listing?.longitude ?? 0,
-  ];
+  const uniqueAmenities = useMemo(() => {
+    return Array.from(new Set(listing.amenities));
+  }, [listing.amenities]);
 
   return (
     <FlexColumn className="listing-overview-data">
-      <Typography variant="h2">{listing?.name ?? 'Name missing 🤷‍♂️'}</Typography>
+      <Typography variant="h2">{listing.name ?? 'Name missing 🤷‍♂️'}</Typography>
 
-      <FlexRow className="listing-overview-data__top-row">
-        <Card className="listing-overview-data__data-item">
-          <CardContent>
-            <Typography
-              className="listing-overview-data__data-item__details-wrapper"
-              variant="body2"
-            >
-              <Typography
-                className="listing-overview-data__data-item__description"
-                component="span"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(
-                    listing?.description ?? 'No description 🤷‍♂️',
-                  ),
-                }}
-              />
-            </Typography>
-          </CardContent>
-        </Card>
+      <FlexRow>
+        <ListingPhotosCarousel
+          className="listing-overview-data__photos-carousel"
+          listing={listing}
+        />
 
-        <Card className={classNames('listing-overview-data__map', 'map-card')}>
-          <CardContent>
-            <MapContainer
-              id="listing-overview-map"
-              center={latLngPosition}
-              zoom={13}
-              scrollWheelZoom={false}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
+        <FlexColumn className="listing-overview-data__details">
+          <Card className="listing-overview-data__data-item">
+            <CardContent>
+              <FlexRow style={{ gap: '1.5rem' }}>
+                <Typography className="listing-overview-data__data-item__beds">
+                  {listing.beds ?? 0} Beds
+                </Typography>
+                <Typography className="listing-overview-data__data-item__baths">
+                  {listing.baths ?? 0} Baths
+                </Typography>
+              </FlexRow>
 
-              <Marker position={latLngPosition}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </MapContainer>
-          </CardContent>
-        </Card>
+              {uniqueAmenities.map((amenity) => {
+                return (
+                  <Typography
+                    key={amenity}
+                    className="listing-overview-data__data-item__amenity"
+                    variant="body2"
+                  >
+                    {amenity}
+                  </Typography>
+                );
+              })}
+            </CardContent>
+          </Card>
+        </FlexColumn>
+      </FlexRow>
+
+      <FlexRow className="listing-overview-data__middle-row">
+        <ListingDescriptionCard description={listing.description} />
+
+        <ListingMapCard
+          latitude={listing.latitude} // force formatting
+          longitude={listing.longitude}
+        />
       </FlexRow>
     </FlexColumn>
   );
