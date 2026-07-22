f08b4c1fc5a6a17787c2b879131c868f0c5a2069
Andy Maskiell <maskiella@gmail.com>
2026-07-21 20:34:57 -0400

    feat: add mocks for browser APIs used by embla-carousel

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -11,3 +11,39 @@ const mockLocation = vi.fn(() => {
 })();
 
 vi.stubGlobal('location', mockLocation);
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+class IntersectionObserver {
+  observe = vi.fn();
+  disconnect = vi.fn();
+  unobserve = vi.fn();
+}
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
+});
+
+class ResizeObserver {
+  observe = vi.fn();
+  unobserve = vi.fn();
+  disconnect = vi.fn();
+}
+Object.defineProperty(window, 'ResizeObserver', {
+  writable: true,
+  configurable: true,
+  value: ResizeObserver,
+});
