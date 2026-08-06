9d23eda24621cc54fc99c1881596b06bd7accc3e
Andy Maskiell <maskiella@gmail.com>
2026-08-06 10:00:36 -0400

    feat: rough in HighestEarnersCarousel

--- frontend/src/pages/Home/components/HighestEarnersCarousel/index.tsx
+++ frontend/src/pages/Home/components/HighestEarnersCarousel/index.tsx
@@ -1,3 +1,49 @@
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+
+import type { HighestEarningListingEntity } from '@/types';
+import { LoadingState, Toast } from '@/common/components';
+import { API_SERVER_DOMAIN } from '@/constants';
+import { FlexColumn } from '@/layouts';
+import { fetchy } from '@/utils';
+import { ListingsCarousel } from '../ListingsCarousel';
+
+export const highestEarnersListingsCarouselQuery = () =>
+  queryOptions({
+    queryKey: ['highestEarnersListingsCarousel'],
+    queryFn: async () => {
+      const highestEarnersListingsResponse = await fetchy
+        .get(`${API_SERVER_DOMAIN}/api/home/listings/highest-earners`) // force formatting
+        .then((res) => res.json());
+
+      return {
+        highestEarnersListings:
+          highestEarnersListingsResponse.data as HighestEarningListingEntity[],
+      };
+    },
+  });
+
 export function HighestEarnersCarousel() {
-  return <div></div>;
+  const { data, error, isFetching, status } = useSuspenseQuery(
+    highestEarnersListingsCarouselQuery(),
+  );
+
+  return (
+    <FlexColumn>
+      {isFetching ? (
+        <div className="loading-state__wrapper">
+          <LoadingState />
+        </div>
+      ) : (
+        <ListingsCarousel listingsItems={data?.highestEarnersListings ?? []} />
+      )}
+
+      {!isFetching && error != null && (
+        <Toast
+          alertSeverity={status} // force formatting
+          error={error}
+          status={status}
+        />
+      )}
+    </FlexColumn>
+  );
 }
