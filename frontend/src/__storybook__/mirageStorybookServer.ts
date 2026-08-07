a2dc1522fc21f87822839fd1b6176fe8bdf10231
Andy Maskiell <maskiella@gmail.com>
2026-08-07 09:29:30 -0400

    feat: fix home page endpoints in frontend mock server

--- frontend/src/__storybook__/mirageStorybookServer.ts
+++ frontend/src/__storybook__/mirageStorybookServer.ts
@@ -21,9 +21,9 @@ export const createMirageStorybookServer = ({
 
       for (const config of endpointConfigs) {
         const routePath = buildMirageStorybookRoutePath(config);
-        // console.log({
-        //   routePath,
-        // });
+        console.log({
+          routePath,
+        });
 
         this.get(`/${routePath}`, async (schema, request) => {
           const endpointPathWithParams = Object.entries(request.params).reduce(
@@ -31,12 +31,12 @@ export const createMirageStorybookServer = ({
               path.replace(`:${paramKey}`, String(paramValue)),
             routePath,
           );
-          // console.log({
-          //   requestUrl: request.url,
-          //   requestParams: request.params,
-          //   routePath,
-          //   endpointPath: endpointPathWithParams,
-          // });
+          console.log({
+            requestUrl: request.url,
+            requestParams: request.params,
+            routePath,
+            endpointPath: endpointPathWithParams,
+          });
           return getRequestFactory({ endpointPath: endpointPathWithParams, request });
         });
       }
@@ -77,10 +77,12 @@ async function getRequestFactory({
 }
 
 function buildMirageStorybookRoutePath(config: EndpointConfig): string {
+  if (config.fullPath != null) {
+    return config.fullPath;
+  }
+
   const resolvedTypeComponent = (function () {
     switch (config.type) {
-      case 'home':
-        return config.fullPath as string;
       case 'overview':
         return `:${config.entityIdPathParam ?? 'entityId'}`;
       case 'list':
--- frontend/src/mock-data-server/utils/routing.ts
+++ frontend/src/mock-data-server/utils/routing.ts
@@ -1,10 +1,12 @@
 import type { EndpointConfig } from '@/types';
 
 export function buildRoutePath(config: EndpointConfig): string {
+  if (config.fullPath != null) {
+    return config.fullPath;
+  }
+
   const resolvedTypeComponent = (function () {
     switch (config.type) {
-      case 'home':
-        return config.fullPath as string;
       case 'overview':
         return ':entityId';
       case 'list':
