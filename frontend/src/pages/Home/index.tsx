cb0b7b4c4624aa1e0e9c859464f48e61da7dd21a
Andy Maskiell <maskiella@gmail.com>
2026-08-09 13:03:36 -0400

    feat: add placeholder box to Home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,59 +1,41 @@
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { Box, Paper } from '@mui/material';
 
-import { LoadingState } from '@/common/components';
-import { FlexColumn } from '@/layouts';
+import { FlexColumn, FlexRow } from '@/layouts';
 import { TrendsRow } from './components';
 
 import './styles.scss';
 
-function useTestQuery() {
-  return useQuery({
-    queryKey: ['testQuery'],
-    queryFn: async () => {
-      const testData =
-        JSON.parse(window.localStorage.getItem('testData') ?? 'null') ?? [];
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      return { testData };
-    },
-  });
-}
-
 export function Home() {
-  const { data, error, isFetching, status } = useTestQuery();
-
-  const [count, setCount] = useState(0);
-
-  function handleCounterClick(
-    event: React.MouseEvent<HTMLButtonElement>, // force formatting
-  ) {
-    const newCount = count + 1;
-    setCount(newCount);
-    data?.testData.push(newCount);
-    window.localStorage.setItem('testData', JSON.stringify(data?.testData ?? []));
-  }
-
   return (
     <FlexColumn id="home" className="home">
       <h2>{`🏡 Home 🏡`}</h2>
 
       <TrendsRow className="home__top-row" />
 
-      <button // force formatting
-        onClick={handleCounterClick}
-      >
-        count is {count}
-      </button>
-
-      <FlexColumn>
-        {isFetching ? (
-          <LoadingState />
-        ) : (
-          <pre>
-            <code>{JSON.stringify(data?.testData ?? [], null, 2)}</code>
-          </pre>
-        )}
-      </FlexColumn>
+      <FlexRow style={{ width: '100%' }}>
+        <Paper
+          style={{
+            height: '30rem',
+            padding: '1rem',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <Box
+            style={{
+              alignItems: 'center',
+              border: '0.125rem dotted var(--mui-palette-primary-main)',
+              display: 'flex',
+              height: '100%',
+              justifyContent: 'center',
+              padding: '1rem',
+              textAlign: 'center',
+            }}
+          >
+            {`Maybe a graph or table?`}
+          </Box>
+        </Paper>
+      </FlexRow>
     </FlexColumn>
   );
 }
