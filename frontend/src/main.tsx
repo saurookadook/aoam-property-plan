11248e08abc303563d5c2f091bdee73d1888478e
Andy Maskiell <maskiella@gmail.com>
2026-08-30 17:13:26 -0400

    bug: fix issue with leaflet assets in prod build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,24 @@
 import { StrictMode, type PropsWithChildren } from 'react';
 import { createRoot } from 'react-dom/client';
+import L from 'leaflet';
+import markerIconUrl from 'leaflet/dist/images/marker-icon.png';
+import markerIconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
-import 'leaflet/dist/leaflet.css';
-import '@/index.scss';
 import App from '@/app/App';
 import { log } from '@/logger';
 import { AppThemeProvider, QueryProviderWrapper } from '@/providers';
 import { AppStateProvider } from '@/store';
+
+import 'leaflet/dist/leaflet.css';
+import '@/index.scss';
+
+L.Icon.Default.imagePath = '';
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIconUrl,
+  iconRetinaUrl: markerIconRetinaUrl,
+  shadowUrl: markerShadowUrl,
+});
 
 function InitApp({ children }: PropsWithChildren) {
   const enableStrictMode = window.localStorage.getItem('aoamStrictMode') === 'true';
